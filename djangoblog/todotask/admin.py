9e3094b1571db7936ec2a37a04ff5cafc7038818
mugao <45284887+mugao-c@users.noreply.github.com>
2022-01-21 18:12:39 +0800

    修复在windows上报错的问题

--- djangoblog/todotask/admin.py
+++ djangoblog/todotask/admin.py
@@ -1,4 +1,4 @@
-from termios import INLCR
+
 from django.contrib import admin
 from .models import Todo, Task
 
@@ -9,14 +9,13 @@ class TodoInline(admin.TabularInline):
     model = Todo
     fk_name = 'parent_task'
     list_display = ('title', 'progress_status')
-    exclude = ('c_time')
+
 
 class TodoAdmin(admin.ModelAdmin):
     list_display = ('title', 'progress_status')
-    exclude = ('c_time')
 
 class TaskAdmin(admin.ModelAdmin):
     inlines = [TodoInline]
     list_display = ('title', 'progress')
-    exclude = ('c_time')
+
 
