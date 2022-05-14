3359e10f5d0e8154ab817a3930441ca6faf9d43d
mugao-c <wangbaiair@163.com>
2022-05-14 23:07:05 +0800

    Update release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -28,8 +28,8 @@ sudo apt-get install jq
 
 check_status "安装json解析库失败"
 
-site_name = cat $work_path/conf/settings.json | jq .website
+site_name = `cat $work_path/conf/settings.json | jq .website`
 
-nginx_file = $work_path/temp/$site_name.conf
+nginx_file = '${work_path}/temp/${site_name}.conf'
 
 echo $nginx_file
