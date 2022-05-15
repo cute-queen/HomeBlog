109b34f18297983c63102ef917ca51314ae2d172
mugao-c <wangbaiair@163.com>
2022-05-15 20:23:10 +0800

    Update settings.py

--- djangoblog/djangoblog/settings.py
+++ djangoblog/djangoblog/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 import sys
 from pathlib import Path
-from django.urls.conf import path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
